4f99de39752b39b0d216f03006a17d1d77f0c1a1
Mohammad Mehdi Mostatabi <mohammadmehdi.mst@outlook.com>
2022-10-10 12:01:42 +0330

    Implement About Me Component

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { PreloaderComponent } from 'src/app/shared/preloader/preloader.component';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
 })
 export class AboutMeComponent implements OnInit {
-  constructor() {}
+  public hide: boolean;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.hide = PreloaderComponent.flag;
+  }
 }
