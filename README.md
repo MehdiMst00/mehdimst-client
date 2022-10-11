32067573536ad1f8bdc7c7c7e026c24cfc8245f7
Mohammad Mehdi Mostatabi <mohammadmehdi.mst@outlook.com>
2022-10-12 01:07:19 +0330

    Implement Resume Component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MehdimstAngular
+# Mehdimst Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PreloaderComponent } from 'src/app/shared/preloader/preloader.component';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
 })
-export class ResumeComponent implements OnInit {
+export class ResumeComponent implements OnInit, AfterViewInit {
   public hide: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
     this.hide = PreloaderComponent.flag;
+  }
+
+  ngAfterViewInit(): void {
+    const delayTime: number = this.hide ? 1000 : 2000;
+    setTimeout(() => {
+      const allProgress = document.getElementsByClassName('animated-progress');
+      for (let index = 0; index < allProgress.length; index++) {
+        const progress: any = allProgress[index];
+        const percent = progress.getAttribute('data-progress');
+        progress.style.width = percent + '%';
+      }
+    }, delayTime);
   }
 }
